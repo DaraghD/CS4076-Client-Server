0f4dcd4efe42db9e401eef77c42f4963a386f11d
daragh <daraghdownes12@gmail.com>
2024-04-25 17:08:52 +0100

    early lecture maybe done

---
+++ src/main/java/dddq/server/EarlyLectureThread.java
@@ -0,0 +1,77 @@
+package dddq.server;
+
+import dddq.client.IncorrectActionException;
+import dddq.client.ScheduleDay;
+import dddq.client.TimeSlot;
+
+public class EarlyLectureThread implements Runnable {
+    private String[] timeArray = {
+            "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30",
+            "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30",
+            "17:00", "17:30", "18:00"
+    };
+    ScheduleDay day;
+    String sDay;
+
+    EarlyLectureThread(ScheduleDay day, String dayString) {
+        this.day = day;
+        this.sDay = dayString;
+    }
+
+    @Override
+    public void run() {
+        for (String time : timeArray) {
+            TimeSlot current = day.getTimeSlot(time);
+            String currentRoom = current.getRoom();
+            ScheduleDay roomTimetable = Server.roomTimetable.get(sDay).get(currentRoom);
+
+            if (current.isTaken()) { // timeslot free for module and ROOM
+                String earliestTime = findEarliestFreeTime(time, roomTimetable);
+                if(earliestTime == null){
+                    continue;
+                }
+
+                int indexEarliest = getIndex(earliestTime);
+                int indexCurrent = getIndex(time);
+                if (indexCurrent > indexEarliest) {
+                    try {
+                        shiftTimeSlot(time, earliestTime, roomTimetable);
+                    } catch (IncorrectActionException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+
+    String findEarliestFreeTime(String time, ScheduleDay roomTimetable) {
+        // check room timetable aswell?
+        for (String t : timeArray) {
+            if (!day.getTimeSlot(t).isTaken() && !roomTimetable.getTimeSlot(t).isTaken()) {
+                return t;
+            }
+        }
+        return null; // they are all taken
+    }
+
+    int getIndex(String time) {
+        for (int i = 0; i < timeArray.length; i++) {
+            if (timeArray[i].equals(time)) {
+                return i;
+            }
+        }
+        return -1; // should never happen
+    }
+
+    void shiftTimeSlot(String current, String newTime, ScheduleDay roomTimetable) throws IncorrectActionException {
+        day.getTimeSlot(newTime).setModule(day.getTimeSlot(current).getModule());
+        day.getTimeSlot(newTime).setRoom(day.getTimeSlot(current).getRoom());
+        day.getTimeSlot(newTime).takeSlot();
+        day.getTimeSlot(current).freeSlot();
+
+        roomTimetable.getTimeSlot(newTime).setModule(day.getTimeSlot(current).getModule());
+        roomTimetable.getTimeSlot(newTime).setRoom(day.getTimeSlot(current).getRoom());
+        roomTimetable.getTimeSlot(newTime).takeSlot();
+        roomTimetable.getTimeSlot(current).freeSlot();
+    }
+}
--- src/main/java/dddq/server/EarlyLectures.java
+++ src/main/java/dddq/server/EarlyLectures.java
@@ -1,38 +1,42 @@
 package dddq.server;
 
-import dddq.client.ScheduleDay;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.RecursiveAction;
 
-import java.util.concurrent.*;
+public class EarlyLectures extends RecursiveAction {
+    Programme programme;
+    int start;
+    int end;
+    CopyOnWriteArrayList<Module> modules;
+    static final int SEQUENTIAL_CUTOFF = 1000; // Threshold for sequential processing
 
-public class EarlyLectures extends RecursiveTask<Boolean> {
-
-        Module module;
-
-        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-
-        ConcurrentHashMap<String, ScheduleDay> earlyLectures;
-
-        EarlyLectures(Module m){
-            this.module = m;
-            earlyLectures = module.getClasses();
-        }
-
-        protected Boolean compute(){
-            for(String day : days){
-
-            }
-
-
-            return false;
-        }
-
-
-    public boolean earlyLectureModule(ScheduleDay day){
-        //
-
-        return false;
-
+    EarlyLectures(Programme p, int start, int end) {
+        this.programme = p;
+        this.start = start;
+        this.end = end;
+        this.modules = p.getModules();
     }
 
+    protected void compute() {
+        // Base case: If the range of modules is below the sequential cutoff, process them sequentially
+        if (end - start < SEQUENTIAL_CUTOFF) {
+            for (int i = start; i < end; i++) {
+                Module module = modules.get(i);
+                earlyModule(module);
+            }
+        } else {
+            int mid = (start + end) / 2;
+            EarlyLectures left = new EarlyLectures(programme, start, mid);
+            EarlyLectures right = new EarlyLectures(programme, mid, end);
+            left.fork();
+            right.compute();
+            left.join();
+        }
+    }
 
+    public void earlyModule(Module m) {
+
+
+    }
 }
+
